3224622e58a281e0a2bda6d7641a973de4bbf9c2
Steve Yim <stevejyim@gmail.com>
2014-07-26 13:00:07 -0500

    update teh routing for a new page

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
-var triviaApp = angular.module('triviaApp', []);
+var triviaApp = angular.module('triviaApp', ['firebase', 'ngRoute']);
 
-triviaApp.controller('QuestionListCtrl', ['$scope', '$http', function($scope, $http) {
-	$http.get('data/questions.json').success(function(data){
-		$scope.questions = data;
-	})
+
+
+
+triviaApp.config(['$routeProvider', function($routeProvider){
+ $routeProvider.
+ 	when('/questions', {
+ 		templateUrl: 'partials/question-list.html',
+ 		controller: 'QuestionListCtrl'
+ 	}).
+ 	when('/questions/new',{
+ 		templateUrl: 'partial/question-form.html',
+ 		controller: 'QuestionListCtrl'
+ 	}).
+ 	otherwise({
+ 		redirectTo: '/questions'
+ 		});
+	}]);
+
+
+triviaApp.controller('QuestionListCtrl', ['$scope', '$firebase', function($scope, $firebase) {
+	var firebaseUrl = "https://blinding-fire-6282.firebaseio.com";
+	$scope.questions = $firebase(new Firebase(firebaseUrl));
 }]);
+

85ee04a1302859f1a4ca3c01af3d3c75e53ec7ac
Steve Yim <stevejyim@gmail.com>
2014-07-26 13:53:04 -0500

    updated added 2nd partial form

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
 var triviaApp = angular.module('triviaApp', ['firebase', 'ngRoute']);
 
-
-
-
 triviaApp.config(['$routeProvider', function($routeProvider){
  $routeProvider.
  	when('/questions', {
@@ -10,7 +7,7 @@ triviaApp.config(['$routeProvider', function($routeProvider){
  		controller: 'QuestionListCtrl'
  	}).
  	when('/questions/new',{
- 		templateUrl: 'partial/question-form.html',
+ 		templateUrl: 'partials/question-form.html',
  		controller: 'QuestionListCtrl'
  	}).
  	otherwise({
@@ -19,8 +16,15 @@ triviaApp.config(['$routeProvider', function($routeProvider){
 	}]);
 
 
-triviaApp.controller('QuestionListCtrl', ['$scope', '$firebase', function($scope, $firebase) {
-	var firebaseUrl = "https://blinding-fire-6282.firebaseio.com";
-	$scope.questions = $firebase(new Firebase(firebaseUrl));
+triviaApp.controller('QuestionListCtrl', ['$scope', '$firebase', '$location', function($scope, $firebase, $location) {
+	$scope.question = {};
+
+	$scope.persistQuestion = function(question) {
+		var firebaseUrl = "https://blinding-fire-6282.firebaseio.com/questions";
+		$scope.questions = $firebase(new Firebase(firebaseUrl));
+		$scope.questions.$add(question).then(function(ref){
+			$location.url('/questions');
+		});
+	};
 }]);
 

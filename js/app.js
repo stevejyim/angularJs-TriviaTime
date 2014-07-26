ad9f1b828185cfd4656d81e6ea7ec96acd6e44eb
Steve Yim <stevejyim@gmail.com>
2014-07-26 11:26:10 -0500

    updated

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var triviaApp = angular.module('triviaApp', []);
+
+triviaApp.controller('QuestionListCtrl', ['$scope', '$http', function($scope, $http) {
+	$http.get('data/questions.json').success(function(data){
+		$scope.questions = data;
+	})
+}]);
